41c75bc91b851749c4160561b4649cc0279f169f
Leandro Bernardo <bernardoleandro713@gmail.com>
2026-05-29 16:42:50 -0300

    ATT CAMERA PARTE 4

--- camera/testes/teste_imagem.py
+++ camera/testes/teste_imagem.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from detectar_queda import DetectorQueda
 
-
 detector = DetectorQueda()
 
 caminho_imagem = "imagens/queda1.jpg"
@@ -22,6 +21,22 @@ queda, mensagem, imagem_resultado = detector.analisar_imagem(imagem)
 print("Queda:", queda)
 print("Mensagem:", mensagem)
 
+# Redimensiona a imagem para exibição
+altura, largura = imagem_resultado.shape[:2]
+
+nova_largura = 900
+nova_altura = int((nova_largura / largura) * altura)
+
+imagem_resultado = cv2.resize(
+    imagem_resultado,
+    (nova_largura, nova_altura)
+)
+
+# Janela redimensionável
+cv2.namedWindow("Resultado", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Resultado", 900, 600)
+
 cv2.imshow("Resultado", imagem_resultado)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
